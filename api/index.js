bc599d4f2846f05512e22091d71556fd6cef597b
odunsikazeem <145531096+odunsikazeem@users.noreply.github.com>
2024-04-05 20:58:42 +0100

    create new post directory

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const User = require('./models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const { ok } = require("assert")
+const cookieParser = require('cookie-parser')
 const app = express()
 
 const salt = bcrypt.genSaltSync(10)
@@ -13,6 +13,7 @@ const secret = 'gsghshssjsyyrruokmavxvsfsvvz'
 const port = process.env.PORT || 4000;
 app.use(cors({credentials:true,origin:'http://localhost:5173'}));
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect(
  'mongodb+srv://odunsikazeem:Myzl57ADoKb9idQX@cluster0.xovwfb2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
@@ -37,7 +38,6 @@ app.post('/login', async (req, res) => {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     
     if (passOk){
-        //islooged in
         jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
             if (err) throw err;
             res.cookie('token', token).json('ok'); 
@@ -49,7 +49,12 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/profile', (req,res) => {
-    res.json(req.cookies)
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+    
 });
 
 app.listen(port, () => {
--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,15 +1,42 @@
-import React from "react"
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function Header() {
+  const [username,setUsername] = useState('')
+
+
+
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+setUsername(userInfo.username)
+      })
+    });
+  }, []);
+
   return (
     <header>
-      <Link to="/" className="logo">MyBlog</Link>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
       <nav>
-        <Link to="/login">Login</Link>
+        {username && (
+          <>
+          <Link to = "/create">Create new post</Link>
+          </>
+        )}
+        {!username && (
+          <>
+          <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
+          </>
+        )}
+        
       </nav>
     </header>
   );
 }
-export default Header
+export default Header;
